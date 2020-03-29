c7daa0b4472184343f1abf58c589b8d8c171642c
Delfad0r <filippo.gianni.baroni@gmail.com>
2020-03-29 03:37:57 +0200

    dom 29 mar 2020, 03.37.57, CEST

---
+++ haskell-src/Source.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE KindSignatures #-}
+
+import GHC.Num
+import GHC.Show
+
+class Functor (f :: * -> *) where
+    map :: (a -> b) -> f a -> f b
+
+
+data Bool = True | False
+    deriving Show
+
+data Maybe a = Nothing | Just a
+    deriving Show
+
+data List a = Nil | Cons a (List a)
+    deriving Show
+
+data Either a b = Left a | Right b
+    deriving Show
+
+data Pair a b = Pair a b
+    deriving Show
+
+
+instance Functor Maybe where
+    map g Nothing = Nothing
+    map g (Just x) = Just (g x)
+
+instance Functor List where
+    map g Nil = Nil
+    map g (Cons x xs) = Cons (g x) (map g xs)
+
+instance Functor (Either a) where
+    map g (Left x) = Left x
+    map g (Right x) = Right (g x)
+
+instance Functor ((->) a) where
+    map g h = g . h
+
+id :: a -> a
+id x = x
+
+const :: a -> b -> a
+const x y = x
+
+const' :: a -> b -> a
+const' x = \y -> x
+
+const'' :: a -> b -> a
+const'' = \x y -> x
+
+(.) :: (b -> c) -> (a -> b) -> a -> c
+g . f = \x -> g (f x)
+
+flip :: (a -> b -> c) -> b -> a -> c
+flip f x y = f y x
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f = \x y -> f y x
+
+
+
